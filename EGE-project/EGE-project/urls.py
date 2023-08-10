8092fc44981fe24f258adf5207f4f6cb55abd245
Ekaterina <katya070706@gmail.com>
2023-08-10 18:36:06 +0300

    Создала проект

--- EGE-project/EGE-project/urls.py
+++ EGE-project/EGE-project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Proj-project project.
+URL configuration for EGE-project-project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
